0d93554ed8af8c9fbeb211be12975b8bb08a0ac7
Alan Mauricio <alanmauricioc@ouitlook.com>
2020-09-28 15:05:02 -0500

    Validation: AuthCredentialsDto, apssword strength

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,16 @@
-export class AuthCredentialsDto{
-    username:string;
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
-    password:string;
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  password: string;
 }
